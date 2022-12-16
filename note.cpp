23431562cc5ffc1fe2a0a161c48a8163ed86f793
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2022-12-16 16:33:36 +0300

    что то

--- note.cpp
+++ note.cpp
@@ -1,6 +1,11 @@
 #include "note.h"
 
-Note::Note()
+Note:: Note(QString author, QDateTime creationDate, QDateTime changeDate, QStringList tags, QString text)
 {
-
+    this -> author = author;
+    this->creationDate = creationDate;
+    this -> changeDate = changeDate;
+    this -> tags = tags;
+    this -> text = text;
+    this -> id = creationDate.toMSecsSinceEpoch();
 }
--- note.h
+++ note.h
@@ -1,11 +1,26 @@
 #ifndef NOTE_H
 #define NOTE_H
 
+#include <QString>
+#include <QDateTime>
+#include <QSqlQuery>
 
 class Note
 {
 public:
-    Note();
+    QString author;
+    QDateTime creationDate;
+    QDateTime changeDate;
+    QStringList tags;
+    QString text;
+    int id;
+
+    Note(QString author, QDateTime creationDate, QDateTime changeDate, QStringList tags, QString text);
+
+    bool serialize(QSqlQuery& query);
+
+    bool refresh(QSqlQuery& query, int id);
+
 };
 
 #endif // NOTE_H
